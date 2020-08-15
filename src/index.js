0172c27240e1d446be38fbe82d62ebc3d0bcf664
Pascal Prins <pascal.prins@foobar-it.com>
2020-08-15 13:41:17 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+// styles for this kit
+import "assets/css/bootstrap.min.css";
+import "assets/scss/now-ui-kit.scss?v=1.4.0";
+import "assets/demo/demo.css?v=1.4.0";
+import "assets/demo/nucleo-icons-page-styles.css?v=1.4.0";
+
+import Home from "views/Home.js";
+import LandingPage from "views/examples/LandingPage.js";
+import LoginPage from "views/examples/LoginPage.js";
+import ProfilePage from "views/examples/ProfilePage.js";
+import NucleoIcons from "./views/NucleoIcons.js";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Switch>
+        <Route path="/index" render={(props) => <Home {...props} />} />
+        <Route
+          path="/nucleo-icons"
+          render={(props) => <NucleoIcons {...props} />}
+        />
+        <Route
+          path="/landing-page"
+          render={(props) => <LandingPage {...props} />}
+        />
+        <Route
+          path="/profile-page"
+          render={(props) => <ProfilePage {...props} />}
+        />
+        <Route
+          path="/login-page"
+          render={(props) => <LoginPage {...props} />}
+        />
+        <Redirect to="/index" />
+        <Redirect from="/" to="/index" />
+      </Switch>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
